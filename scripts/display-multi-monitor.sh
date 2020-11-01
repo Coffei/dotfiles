085c8bb214aee5c7218b27b35bb49ee8d22d603f
Jonas Trantina <jonas.trantina@altworx.com>
2020-11-01 16:36:11 +0100

    Rearrange monitor scripts
    This sequence of commands behaves better.

--- scripts/display-multi-monitor.sh
+++ scripts/display-multi-monitor.sh
@@ -2,11 +2,11 @@
 # Change monitor layout to work setup - two monitors side-by-side. This scales NTB monitor to FullHD
 # (so it matches the DPI of the other monitor) and changes DPI to LOW.
 
-xrandr --output eDP1 --mode 1920x1080
-xrandr --output DP1 --auto --right-of eDP1
-sleep 1
 lodpi
-i3-msg restart
 sleep 1
-~/.config/polybar/launch-all.sh
+xrandr --output eDP-1 --mode 1920x1080
+xrandr --output DP-1 --auto --right-of eDP-1
+sleep 1
+i3-msg restart
+# ~/.config/polybar/launch-all.sh
 pkill dunst && dunst &
--- scripts/display-single-monitor.sh
+++ scripts/display-single-monitor.sh
@@ -2,11 +2,11 @@
 # Change monitor layout to single-NTB monitor. Disconnect all other monitors and set the NTB display
 # to full resolution. This changes DPI to HIGH.
 
-xrandr --output DP1 --off --output DP2 --off
-xrandr --output eDP1 --mode 3200x1800
-sleep 1
 hidpi
-i3-msg restart
 sleep 1
-~/.config/polybar/launch-all.sh
+xrandr --output DP-1 --off --output DP-2 --off
+xrandr --output eDP-1 --mode 3200x1800
+sleep 1
+i3-msg restart
+# ~/.config/polybar/launch-all.sh
 pkill dunst && dunst &
