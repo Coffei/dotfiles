81daf9fdefea40607dc17a9d5e73e1fdbea3a186
Jonas Trantina <jonas.trantina@altworx.com>
2024-03-06 14:31:20 +0100

    Improve polybar kblayout script
    Account for different order of layouts as they sometimes get mixed up.

--- xps13/polybar/kblayout.sh
+++ xps13/polybar/kblayout.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 layout=$(xset -q | grep -A 0 'LED' | cut -c59-67)
+variant_order=$(setxkbmap -query | grep variant | cut -c13-18)
 
-if [[ $layout < 00001000 ]]; then
-    text=en
+if [[ $layout -lt 00001000 ]]; then
+    if [[ $variant_order == 'coder,' ]]; then
+        text=en
+    else
+        text=cs
+    fi
 else
-    text=cs
+    if [[ $variant_order == 'coder,' ]]; then
+        text=cs
+    else
+        text=en
+    fi
 fi
 
 echo $text
